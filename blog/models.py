4dbece86a9dcc1177fff3d13b048c98d6e99d045
Princessb1 <blessing.r.garba@gmail.com>
2022-06-19 17:37:43 +0100

    updated my new app blog

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField()
+
+    def _str_(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
 
